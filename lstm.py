3e8f5d57f1748166bfd82335e592eeeda733cad5
spectre900 <nayakpratham0@gmail.com>
2021-03-07 16:47:01 +0530

    seq2seq using LSTM

--- lstm.py
+++ lstm.py
@@ -6,25 +6,33 @@ import time
 import numpy as np
 import pandas as pd
 import pickle as pkl
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from collections import defaultdict 
 import tensorflow.keras.backend as K
 
-from tensorflow.keras import regularizers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from tensorflow.keras.models import Model
+
 from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Embedding
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Layer
-from tensorflow.keras.layers import RNN
+from tensorflow.keras.layers import TimeDistributed
 
-EPOCHS = 10
-MAX_LEN = 20
-VECTOR_DIM = 100
-BATCH_SIZE = 128
+from tensorflow.keras.optimizers import Adam
 
-def getData(path):
+
+EPOCHS = 15
+MAX_LEN = 50
+VOCAB_SIZE = 0
+VECTOR_DIM = 80
+HIDDEN_DIM = 100
+BATCH_SIZE = 16
+
+def getData(path,limit):
 
     curSent = []
     rawData = []
@@ -47,7 +55,7 @@ def getData(path):
         rawData.append([x[0] for x in curSent])
         normalizedData.append([x[1] for x in curSent])
 
-    return rawData, normalizedData
+    return rawData[:limit], normalizedData[:limit]
 
 def buildDict(data):
 
@@ -79,68 +87,58 @@ def tokenize(data,wordToNum):
 
     return np.array(tokenizedData)
 
-def getModel(wordToNum): 
+def getModel(VOCAB_SIZE):
 
-    dModel = VECTOR_DIM
-    vocabSize = len(wordToNum)
+    encoder_inputs = Input(shape=(MAX_LEN,), dtype='int32')
+    encoder_embedding = Embedding(input_dim=VOCAB_SIZE, output_dim=VECTOR_DIM,input_length=MAX_LEN,trainable=True)(encoder_inputs)
+    encoder_LSTM = LSTM(HIDDEN_DIM, return_state=True)
+    encoder_outputs, state_h, state_c = encoder_LSTM(encoder_embedding)
+    
+    decoder_inputs = Input(shape=(MAX_LEN,), dtype='int32')
+    decoder_embedding =Embedding(input_dim=VOCAB_SIZE, output_dim=VECTOR_DIM,input_length=MAX_LEN,trainable=True)(decoder_inputs)
+    decoder_LSTM = LSTM(HIDDEN_DIM, return_state=True, return_sequences=True)
+    decoder_outputs, _, _ = decoder_LSTM(decoder_embedding, initial_state=[state_h, state_c])
+    
+    outputs = TimeDistributed(Dense(VOCAB_SIZE,activation='softmax'))(decoder_outputs)
 
-    model = Sequential()
+    model = Model([encoder_inputs, decoder_inputs], outputs)
 
-    # PENDING..............
-    # PENDING..............
-    # PENDING..............
-    # PENDING..............
-    # PENDING..............
-    # PENDING..............
-    # PENDING..............
-    # PENDING..............
-    # PENDING..............
+    model.compile(optimizer=Adam(0.01), loss='categorical_crossentropy')
     
     print(model.summary())
     
     return model
 
+
 def trainModel():
 
-    rawDataTrain, normalizedDataTrain = getData('train.norm')
-    rawDataTest, normalizedDataTest = getData('dev.norm')
+    rawDataTrain, normalizedDataTrain = getData('train.norm',800)
+    rawDataTest, normalizedDataTest = getData('dev.norm',200)
 
     wordToNum = buildDict(rawDataTrain+normalizedDataTrain)
+    vocab_size = len(wordToNum)+1
     
     rawDataTrain = tokenize(rawDataTrain, wordToNum)
     normalizedDataTrain = tokenize(normalizedDataTrain, wordToNum)
 
     rawDataTest = tokenize(rawDataTest, wordToNum)
     normalizedDataTest = tokenize(normalizedDataTest, wordToNum)
+    
+    xTrain = rawDataTrain
+    yTrain = normalizedDataTrain
 
-    model       = getModel(wordToNum)
-    
-    xTrain      = rawDataTrain
-    yTrain      = normalizedDataTrain
-    xTest       = rawDataTest
-    yTest       = normalizedDataTest
-    
-    history     = model.fit(xTrain, yTrain, validation_data=(xTest,yTest), epochs=EPOCHS, batch_size=BATCH_SIZE)
+    xTest  = rawDataTest
+    yTest  = normalizedDataTest
+
+    yTrainOneHot = tf.one_hot(yTrain,vocab_size)
+    yTestOneHot  = tf.one_hot(yTest,vocab_size)
+
+    model       = getModel(vocab_size)
+
+    history     = model.fit([xTrain, yTrain], yTrainOneHot, validation_data=([xTest, yTest], yTestOneHot), batch_size=BATCH_SIZE, epochs=EPOCHS)
     
     return model,history
 
-def plot(history):
-    
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy and loss')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.show()
 
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.show()
-
-model,history = trainModel()
-plot(history)
+if __name__ == "__main__":
+    model,history = trainModel()
